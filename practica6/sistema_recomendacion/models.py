528798622d05e56b91227c033bd0f13191c4f53d
joaquinperez94 <32302020+joaquinperez94@users.noreply.github.com>
2018-12-17 12:34:44 +0100

    modelo OK

--- practica6/sistema_recomendacion/models.py
+++ practica6/sistema_recomendacion/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django_mysql.models import EnumField
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import datetime
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Usuario(models.Model):
+    Id = models.CharField(max_length=100, unique=True,primary_key=True)
+    Sexo=EnumField(choices=['M','F'])
+    Ocupacion=models.CharField(max_length=50)
+    CodigoPostal=models.IntegerField(max_length=7)
+
+class Pelicula(models.Model):
+    Id = models.CharField(max_length=100, unique=True, primary_key=True)
+    Titulo = models.CharField(max_length=50)
+    FechaEstrenoVideo=models.CharField(max_length=50)
+    IMDbURL=models.CharField(max_length=50)
+    Categorias=models.CharField(max_length=50)
+
+class Puntuacion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+    puntuacion=models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    fecha = models.DateTimeField(default=datetime.now(), blank=True)
