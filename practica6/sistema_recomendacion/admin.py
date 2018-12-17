f41d98f638e7560eae7c64c253d0169c28e7a1ca
joaquinperez94 <32302020+joaquinperez94@users.noreply.github.com>
2018-12-17 12:47:23 +0100

    añado al admin modelos

--- practica6/sistema_recomendacion/admin.py
+++ practica6/sistema_recomendacion/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from sistema_recomendacion.models import Usuario,Pelicula,Puntuacion
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Pelicula)
+admin.site.register(Puntuacion)
+
